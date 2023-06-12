568d970d9eae5aeef958f7816133c81403f570f8
Ezequiel Scigolini <escigolini@gmail.com>
2023-06-12 22:55:17 +0200

    Commit finish with ethers.js. Coming next Hardhat.

--- encryptKey.ts
+++ encryptKey.ts
@@ -1,24 +1,23 @@
-import { ethers } from "ethers";
-import fs from "fs-extra";
-import * as dotenv from "dotenv";
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main() {
-  dotenv.config();
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
-  // const pv = process?.env.PRIVATE_KEY ?? "";
-  const encryptedJsonKey = await ethers.encryptKeystoreJson(
-    wallet,
-    process.env.PRIVATE_KEY_PASSWORD ?? ""
-  );
-  console.log(encryptedJsonKey);
-  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!)
+    // const pv = process?.env.PRIVATE_KEY ?? "";
+    const encryptedJsonKey = await ethers.encryptKeystoreJson(
+        wallet,
+        process.env.PRIVATE_KEY_PASSWORD!
+    )
+    console.log(encryptedJsonKey)
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
 
-main();
+main()
